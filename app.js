4365d886349975962557ba21eff61341d973f3a6
008008029 <008008029@N210C3>
2022-01-24 20:11:40 -0500

    fixed port for real this time

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { request } = require("express")
 const express = require("express")
 const exphbs = require("express-handlebars")
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 const app = express()
 
 //Set the view engine
